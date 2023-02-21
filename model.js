08818ef9669b6bf300d8b3f5828c63ff168cbaea
alisson10000 <alisson_lima_souza@yahoo.com.br>
2023-02-21 17:40:15 -0200

    Atualiza o json
    atualização das dependências

--- model.js
+++ model.js
@@ -1,10 +1,25 @@
+//Constante abaixo crio com a letra maiúscula
+
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('asdfas','root','',{
+//=====================================================================
+
+// Criar uma instancia para a constante sequelize
+//passando o nome do banco, usuario e senha , 
+
+
+const sequelize = new Sequelize('banco','root','',{
     host:"localhost",
     dialect:'mariadb'
 })
+
+/**
+ * Abaixo eu crio uma função que faça tratamento se 
+ * o banco de dados foi conectado aou não
+ */
+
 sequelize.authenticate(function(){
     console.log("erro")
 }).catch(function(erro){
     console.log("Falha ao conectar" + erro)
 })
+//========================================================================
