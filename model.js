be3668d0fa4178832587c4e9875c36c02aa9c3ce
alisson10000 <alisson_lima_souza@yahoo.com.br>
2023-02-21 17:50:56 -0200

    Update model.js

--- model.js
+++ model.js
@@ -15,10 +15,12 @@ const sequelize = new Sequelize('banco','root','',{
 /**
  * Abaixo eu crio uma função que faça tratamento se 
  * o banco de dados foi conectado aou não
+ * 
+ * comportamento de função assincrona
  */
 
-sequelize.authenticate(function(){
-    console.log("erro")
+sequelize.authenticate().then(function(){
+    console.log("Ok")
 }).catch(function(erro){
     console.log("Falha ao conectar" + erro)
 })
